c2e35933f7ea3af7c613f4735b426044f836afa8
LENOVO <LENOVO@DESKTOP-LOLKBPC>
2026-01-16 10:41:57 +0400

    Add project files.

---
+++ FSLesson8/Extentions/Extentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSLesson8.Extentions
+{
+    public static class Extentions
+    {
+        public static int MySum(this int num1,int num2)
+        {
+            int result = 1;
+            for(int i = num2; i >= 1; i--)
+            {
+                result *= num1;
+            }
+            return result;
+        }
+    }
+}
---
+++ FSLesson8/Program.cs
@@ -0,0 +1,19 @@
+﻿using FSLesson8.Extentions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSLesson8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            Console.WriteLine(num1.MySum(num2));
+        }
+    }
+}
